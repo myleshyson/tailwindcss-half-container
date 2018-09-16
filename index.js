9b867cfecee04107e3ffacfa88413c20130d2530
Myles Hyson <mylesshyson@gmail.com>
2018-09-15 23:02:10 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const _ = require('lodash');
+const strip = require('css-strip-units');
+
+function extractMinWidths(breakpoints) {
+  return _.flatMap(breakpoints, breakpoints => {
+    if (_.isString(breakpoints)) {
+      breakpoints = { min: breakpoints }
+    }
+
+    if (!_.isArray(breakpoints)) {
+      breakpoints = [breakpoints]
+    }
+
+    return _(breakpoints)
+      .filter(breakpoint => {
+        return _.has(breakpoint, 'min') || _.has(breakpoint, 'min-width')
+      })
+      .map(breakpoint => {
+        return _.get(breakpoint, 'min-width', breakpoint.min)
+      })
+      .value()
+  })
+}
+
+module.exports = function(options) {
+  return function({addComponents, config}) {
+    const screens = _.get(options, 'screens', config('screens'))
+    const minWidths = extractMinWidths(screens);
+
+    const atRules = _.map(minWidths, minWidth => {
+      const unit = strip(minWidth);
+      return {
+        [`@media (min-width: ${minWidth})`]: {
+          '.half-container': {
+            'max-width': `${parseInt(minWidth, 10) / 2}${unit}`,
+          },
+        },
+      }
+    })
+    addComponents([
+      {
+        '.half-container': Object.assign(
+          {'width': '100%'},
+          _.get(options, 'center', false) ? { marginRight: 'auto', marginLeft: 'auto' } : { marginLeft: 'auto'},
+          _.has(options, 'padding')
+            ? { paddingRight: options.padding, paddingLeft: options.padding }
+          : {}
+        )
+      },
+      {
+        '.half-container--end': Object.assign(
+          _.get(options, 'center', false) ? { marginRight: 'auto', marginLeft: 'auto' } : { marginRight: 'auto', marginLeft: 'initial'},
+        )
+      },
+      ...atRules
+    ]);
+  }
+}
